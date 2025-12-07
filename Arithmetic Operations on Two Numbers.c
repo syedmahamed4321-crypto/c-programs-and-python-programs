79ffe150425870c5541db7f06a407d1a352d694f
syed mahamad thouhid <syedmahamed4321@gmail.com>
2025-12-07 23:35:48 +0530

    Arithmetic Operations on Two Numbers.c

---
+++ Arithmetic Operations on Two Numbers.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    float a=1.8, b=4.2;   // Using float to handle decimals
+
+    printf("\nAddition = %.2f", a + b);
+    printf("\nSubtraction = %.2f", a - b);
+    printf("\nMultiplication = %.2f", a * b);
+
+    if (b != 0) {
+        printf("\nDivision = %.2f", a / b);
+    } else {
+        printf("\nDivision not possible (cannot divide by zero)");
+    }
+
+    return 0;
+}
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    float a=1.8, b=0;   // Using float to handle decimals
+
+    printf("\nAddition = %.2f", a + b);
+    printf("\nSubtraction = %.2f", a - b);
+    printf("\nMultiplication = %.2f", a * b);
+
+    if (b != 0) {
+        printf("\nDivision = %.2f", a / b);
+    } else {
+        printf("\nDivision not possible (cannot divide by zero)");
+    }
+
+    return 0;
+}
