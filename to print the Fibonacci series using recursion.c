b034a6c2ae89aee5f79c7d1e7463fdbaa4091102
syed mahamad thouhid <syedmahamed4321@gmail.com>
2025-12-22 15:06:45 +0530

    to print the Fibonacci series using recursion.c

--- to print the Fibonacci series using recursion.c
+++ to print the Fibonacci series using recursion.c
@@ -1,28 +1,17 @@
 #include <stdio.h>
-int fibonacci(int n)
-{
-    if (n <= 1)
-        return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
-}
-int fibonacci_iteration(int n)
-{
-    if (n <= 1)
-        return 1;
-    int arr[n + 1];
-    arr[0] = 1;
-    arr[1] = 1;
-    for (int i = 2; i < n + 1; i++)
-        arr[i] = arr[i - 1] + arr[i - 2];
-    return arr[n];
-}
 int main()
 {
-    int n = 9;
-    printf("Fibonacci using recursion of %d:%d\n", n,
-           fibonacci(n));
-    n = 11;
-    printf("Fibonacci using iteration of %d:%d", n,
-           fibonacci_iteration(n));
+    int n, i;
+    int a = 0, b = 1, c;
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series: ");
+    for (i = 1; i <= n; i++)
+    {
+        printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
     return 0;
 }
