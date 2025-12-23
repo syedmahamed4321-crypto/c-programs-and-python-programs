445b2382eb08a6f934edc3f3dece2dd481d93791
syed mahamad thouhid <syedmahamed4321@gmail.com>
2025-12-24 00:27:09 +0530

    if else if.c

---
+++ if else if.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a=10;
+if(a==10)
+{
+printf("value is equal to 10");
+}
+else if(a==20)
+{
+printf("value is equal to 20");
+}
+else if(a==30)
+{
+printf("value is equal to 30");
+}
+else
+printf("number not found");
+return 0;
+}
