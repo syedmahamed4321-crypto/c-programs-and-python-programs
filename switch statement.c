361943f49867d909d51d6d8b4552b8aad00d7d40
syed mahamad thouhid <syedmahamed4321@gmail.com>
2025-12-24 00:45:13 +0530

    switch statement.c

---
+++ switch statement.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a=0;
+switch(a)
+{
+case1:
+printf("number is equal to 1");
+break;
+case2:
+printf("number is equal to 2");
+break;
+case3:
+printf("number is equal to 3");
+break;
+default:
+printf("number not found");
+}
+return 0;
+}
