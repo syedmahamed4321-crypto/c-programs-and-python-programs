048868e882ef60bad893b0b3cb26b9a2a0ab93e4
syed mahamad thouhid <syedmahamed4321@gmail.com>
2025-12-23 23:09:49 +0530

    sum of 3 avg num.c

---
+++ sum of 3 avg num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,sum;
+float avg;
+a=46;
+b=55;
+c=50;
+sum=a+b+c;
+avg=(a+b+c)/3;
+printf("the sum of three numbers is :%d\n",sum);
+printf("the average of three numbers is :%d\n",avg);
+}
