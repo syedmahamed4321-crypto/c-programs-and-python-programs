f648ac2f0feb8dd6ff76b979052aeadae882cd80
syed mahamad thouhid <syedmahamed4321@gmail.com>
2025-12-23 23:50:33 +0530

    implict type casting.c

---
+++ implict type casting.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a=10;
+    float b=5.5;
+    double res;
+    res=a+b;
+    printf("integer value %d\n",a);
+    printf("float value %2f\n",b);
+    printf("result after implict conversion:%2f\n",res);
+}
