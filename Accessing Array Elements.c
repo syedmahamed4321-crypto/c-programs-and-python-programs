48562acb28b643dea6d80802e842fa603de546eb
syed mahamad thouhid <syedmahamed4321@gmail.com>
2025-12-24 15:28:20 +0530

    Accessing Array Elements.c

---
+++ Accessing Array Elements.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    // array declaration and initialization
+    int arr[5] = {2, 4, 8, 12, 16};
+    // accessing element at index 2 i.e 3rd element
+    printf("%d\n ", arr[2]);
+    // accessing element at index 4 i.e last element
+    printf("%d\n ", arr[4]);
+    // accessing element at index 0 i.e first element
+    printf("%d\n ", arr[0]);
+    return 0;
+}
