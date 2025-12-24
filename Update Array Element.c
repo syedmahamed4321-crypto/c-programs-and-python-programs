9c6bda4ed77af016878412c9d4df49a80d9da00a
syed mahamad thouhid <syedmahamed4321@gmail.com>
2025-12-24 15:39:21 +0530

    Update Array Element.c

---
+++ Update Array Element.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int arr[5] = {2, 4, 8, 12, 16};
+    // Update the first value
+    // of the array
+    arr[0] = 1;
+    printf("%d", arr[0]);
+    return 0;
+}
