daa73d78f61be9e43ea5ca765513f12fab8919e4
syed mahamad thouhid <syedmahamed4321@gmail.com>
2025-12-24 14:54:30 +0530

    arrays declaration.c

---
+++ arrays declaration.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int arr[] = {2, 4, 8, 12, 16, 18};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
