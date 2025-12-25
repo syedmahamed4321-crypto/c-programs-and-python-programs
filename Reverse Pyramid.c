74f58ff1fc2ea159c1f9d34abeeb6e9e53e92672
syed mahamad thouhid <syedmahamed4321@gmail.com>
2025-12-25 22:06:39 +0530

    Reverse Pyramid.c

---
+++ Reverse Pyramid.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int N = 5;
+    for (int i = N; i >= 1; i--) {
+        for (int j = 1; j <= N - i; j++)
+            printf(" ");
+        for (int j = 1; j < 2 * i; j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
