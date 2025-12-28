0c967bf26d324d9b9dbba4c93211f2b9b9f59d0c
syed mahamad thouhid <syedmahamed4321@gmail.com>
2025-12-28 20:52:33 +0530

    Area of a Right-Angled Triangle.c

---
+++ Area of a Right-Angled Triangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float base, height, area;
+    printf("Enter base of the triangle: ");
+    scanf("%f", &base);
+    printf("Enter height of the triangle: ");
+    scanf("%f", &height);
+    area = 0.5 * base * height;
+    printf("Area of the right angled triangle = %.2f", area);
+    return 0;
+}
